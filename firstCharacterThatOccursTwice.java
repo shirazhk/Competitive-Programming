b5c515f11ab9f79620c2df5971bdc3a59bd6d909
Shiraz Hasan Khan <35097477+shirazhk@users.noreply.github.com>
2021-11-30 12:08:31 +0530

    Print first character that occurs twice in a word
    This problem has been solved using HashMap Collection data type. The character and its frequency is stored in the HasMap. If a character occurs more than once its frequency is updated accordingly. Then another loop goes through the map and checks for the character that has frequency=2. When the first character with frequency 2 is found, the loop prints result and terminates. If no character occurs twice in the string, an appropriate message is printed.

---
+++ firstCharacterThatOccursTwice.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class firstCharacterThatOccursTwice {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String str = s.next();
+        Map<Character,Integer> map = new HashMap<Character, Integer>();
+        for(int i=0;i<str.length();i++)
+        {
+            if(map.containsKey(str.charAt(i)))
+            {
+                Integer val = map.get(str.charAt(i));
+                map.put(str.charAt(i),val+1);
+            }
+            else
+            {
+                map.putIfAbsent(str.charAt(i), 1);
+            }
+        }//elements put into map with frequency values
+        for(Map.Entry<Character, Integer> e:map.entrySet())
+        {
+            if(e.getValue()==2 && e.getKey()!=str.charAt(str.length()-1))
+            {
+                System.out.println("First character to occur twice: "+e.getKey());
+                break;
+            }
+            else if(e.getKey()==str.charAt(str.length()-1) && e.getValue()!=2)
+            {
+                System.out.println("No element occurs twice");
+            }
+        }
+    }
+}
